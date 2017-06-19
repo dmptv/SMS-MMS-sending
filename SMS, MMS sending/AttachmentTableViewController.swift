cde9f476dd3a56c8ce3f18ff8b32f1e60c602793
dmptv <kanat@Dimas-MacBook-Air.local>
2017-06-19 10:36:32 +0600

    яя

--- SMS, MMS sending/AttachmentTableViewController.swift
+++ SMS, MMS sending/AttachmentTableViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import MessageUI
 
-
 class AttachmentTableViewController: UITableViewController, MFMessageComposeViewControllerDelegate {
     
     let filenames = ["camera-photo-tips.html", "foggy.jpg", "Hello World.ppt", "no more complaint.png", "Just Dev.doc"]
@@ -24,19 +23,13 @@ class AttachmentTableViewController: UITableViewController, MFMessageComposeView
         return filenames.count
     }
     
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = filenames[indexPath.row]
         cell.imageView?.image = UIImage(named: "icon\(indexPath.row).png");
         return cell
     }
-    
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedFile = self.filenames[indexPath.row]
-        
-    }
+
     
     // MARK: - MFMessageComposeViewControllerDelegate
     func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
@@ -52,11 +45,44 @@ class AttachmentTableViewController: UITableViewController, MFMessageComposeView
         dismiss(animated: true, completion: nil)
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedFile = self.filenames[indexPath.row]
+        sendSMS(attachment: selectedFile)
+    }
+    
+    func sendSMS(attachment: String) {
+        // Проверка на возможность отправки СМС
+        guard MFMessageComposeViewController.canSendText() else {
+            let alert = UIAlertController(title: "SMS Unavailable", message: "Your device is not capable sending SMS", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        // Взяли Url пути
+        let fileparts = attachment.components(separatedBy: ".")
+        let filename = fileparts[0]
+        let fileExtention = fileparts[1]
+        let filePath = Bundle.main.path(forResource: filename, ofType: fileExtention)
+        let fileURL = NSURL(fileURLWithPath: filePath!)
+        
+        // Настроили       messageController
+        let messageController = MFMessageComposeViewController()
+        messageController.messageComposeDelegate = self
+        messageController.recipients = ["656838393", "656838393", "656838393", "656838393"]
+        messageController.body = "Jsu sent \(attachment) to your e-mail" // We can not sent files via SMS
+        
+        // Прикрепили файл
+        messageController.addAttachmentURL(fileURL as URL, withAlternateFilename: nil)
 
+        present(messageController, animated: true, completion: nil)
+    }
 
 
 
 }
+
+
 
 
 
